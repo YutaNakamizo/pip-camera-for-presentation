f7a19b0aad4da5e09bd5fa309c67be57c93faa52
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2021-06-23 14:25:46 +0900

    #1 implemeted PiP

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,13 +1,25 @@
 import React, {
   useEffect,
   useRef,
+  useState,
 } from 'react';
+import {
+  Toolbar,
+  Button,
+} from '@material-ui/core';
 import { Video } from '~/components/Video';
 
 export const Root = ({
   ...props
 }) => {
+  const [ pipEnabled, setPiPEnabled ] = useState(
+    document.pictureInPictureEnabled
+  );
+  const [ isPiP, setIsPiP ] = useState(false);
+
   const videoElRef = useRef();
+
+  // Setup video
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({
       audio: false,
@@ -19,11 +31,41 @@ export const Root = ({
     });
   }, []);
 
+  const switchPiP = (e, _isPiP) => {
+    if(isPiP) { // Exit PiP
+      document.exitPictureInPicture().then(() => {
+        setIsPiP(false);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+    else { // Enter PiP
+      videoElRef.current.requestPictureInPicture().then(pipWindow => {
+        setIsPiP(true);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+  };
+
   return (
     <>
       <Video
         ref={videoElRef}
       />
+
+      <Toolbar
+      >
+        <Button
+          variant="contained"
+          disabled={!pipEnabled}
+          onClick={e => {
+            switchPiP(e, isPiP)
+          }}
+        >
+          {isPiP ? 'Exit PiP' : 'Enable PiP'}
+        </Button>
+      </Toolbar>
     </>
   );
 };
