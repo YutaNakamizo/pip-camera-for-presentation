3b9353c533ea194b799617ae332e580b8cdefdba
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2021-06-23 13:51:20 +0900

    init basic packages

--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+} from 'react-router-dom';
+import {
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
+import {
+  purple,
+  yellow,
+} from '@material-ui/core/colors';
+import '~/index.css';
+import { App } from '~/App';
+import * as serviceWorkerRegistration from '~/serviceWorkerRegistration';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: yellow,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider
+      theme={theme}
+    >
+      <BrowserRouter
+      >
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
