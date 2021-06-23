c7aa0b51636021de939e193944227e1ece32ec16
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2021-06-23 16:45:55 +0900

    #2 implemented camera switch

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -6,6 +6,8 @@ import React, {
 import {
   Toolbar,
   Button,
+  Select,
+  MenuItem,
 } from '@material-ui/core';
 import { Video } from '~/components/Video';
 
@@ -16,20 +18,49 @@ export const Root = ({
     document.pictureInPictureEnabled
   );
   const [ isPiP, setIsPiP ] = useState(false);
+  const [ videoSources, setVideoSources ]  = useState([]);
+  const [ videoSource, setVideoSource ] = useState();
 
   const videoElRef = useRef();
+
+  // Get devices
+  useEffect(() => {
+    const updateVideoSources = () => {
+      navigator.mediaDevices.enumerateDevices().then(devices => {
+        const sources = devices.filter(device => device.kind === 'videoinput')
+        setVideoSources(sources);
+        if(!sources.find(source => source.deviceId === videoSource)) {
+          setVideoSource(sources[0].deviceId);
+        }
+      }).catch(err => {
+        console.error(err);
+      });
+    };
+    updateVideoSources();
+    navigator.mediaDevices.ondevicechange = updateVideoSources;
+
+    return () => {
+      navigator.mediaDevices.ondevicechange = null;
+    };
+  }, []);
 
   // Setup video
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({
       audio: false,
-      video: true,
+      video: {
+        deviceId: {
+          exact: videoSource,
+        },
+      },
     }).then(mediaStream => {
       videoElRef.current.srcObject = mediaStream;
     }).catch(err => {
       console.error(err);
     });
-  }, []);
+  }, [
+    videoSource,
+  ]);
 
   const switchPiP = (e, _isPiP) => {
     if(isPiP) { // Exit PiP
@@ -56,6 +87,30 @@ export const Root = ({
 
       <Toolbar
       >
+        <Select
+          value={videoSource || '(not-selected)'}
+          onChange={e => {
+            setVideoSource(e.target.value);
+          }}
+        >
+          {videoSources.length > 0 ? (
+            videoSources.map((source, index) => (
+              <MenuItem
+                key={source.deviceId}
+                value={source.deviceId}
+              >
+                {source.label}
+              </MenuItem>
+            ))
+          ) : (
+            <MenuItem
+              value="(not-selected)"
+            >
+              (Not Selected)
+            </MenuItem>
+          )}
+        </Select>
+
         <Button
           variant="contained"
           disabled={!pipEnabled}
